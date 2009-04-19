128f0f82ca261da70e1974a52b63d659ff356154
Johannes Berg <johannes@sipsolutions.net>
2009-04-19 15:05:58 +0200

    add "phy" command as alias for "list"

--- info.c
+++ info.c
@@ -268,3 +268,4 @@ static int handle_info(struct nl80211_state *state,
 }
 TOPLEVEL(info, NULL, NL80211_CMD_GET_WIPHY, 0, CIB_PHY, handle_info);
 TOPLEVEL(list, NULL, NL80211_CMD_GET_WIPHY, NLM_F_DUMP, CIB_NONE, handle_info);
+TOPLEVEL(phy, NULL, NL80211_CMD_GET_WIPHY, NLM_F_DUMP, CIB_NONE, handle_info);
